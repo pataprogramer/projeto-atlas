d5256a518683581f353a7e1efe39db3b4861888b
Luccas <luccas.callabria@gmail.com>
2026-07-06 14:39:43 -0300

    Resposta aos exercícios da Aula 04 do Curso em vídeo

---
+++ curso/aula04.py
@@ -0,0 +1,19 @@
+'''#desafio 01
+nome = input('Qual é o seu nome?: ')
+print(f'Olá {nome} ! Prazer em te conhecer!)
+'''
+
+'''#desafio 02
+dia = input('Que dia você nasceu? ')
+mes = input('Em qual mês você nasceu? ')
+ano = input('Em que ano você nasceu? ')
+
+print(f'você nasceu em {dia}/{mes}/{ano}')
+'''
+
+'''#desafio 03
+x = int(input('digite um número: '))
+y = int(input('digite outro número: '))
+
+print(f'{x+y}')
+'''
